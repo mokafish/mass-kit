2d7baa44f1e77414034597d01bac7a20ad889923
mokafish <mokafish@users.noreply.github.com>
2025-06-30 16:57:50 +0800

    更新play.js和dsl.js文件，添加Interpreter类以支持代码解析和执行，移除未使用的代码并优化Token类构造函数。

--- play.js
+++ play.js
@@ -1,6 +1,11 @@
-import { Lexer } from './lib/dsl.js';
+import { Lexer,Interpreter } from './lib/dsl.js';
 
-let lexer = new Lexer('{name} is {age} years old');
-lexer = new Lexer('{{name}} is age years {1} {old');
-let tokens = lexer.tokenize();
-console.log(tokens);
+// let lexer = new Lexer('{name} is {age} years old');
+// lexer = new Lexer('{{name}} is age years {1} {old');
+// let tokens = lexer.tokenize();
+// console.log(tokens);
+
+let itp = new Interpreter()
+itp.load('{name} is {age} years 12/24 old')
+console.log(itp.interpret());
+
