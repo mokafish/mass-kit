61d1b8abf045613ac8e86bf853e1a3b23b3ef442
mokafish <mokafish@users.noreply.github.com>
2025-07-03 09:34:33 +0800

    重构Parser和Interpreter类，移除AST类并直接使用数组存储节点；新增InterpreterError类以处理解释器错误；更新测试用例以反映这些更改并验证功能。

--- test.js
+++ test.js
@@ -15,12 +15,13 @@ test('dsl.Lexer', async t => {
         `hello {exec abc "x y z"} ... , {"exec 2" 'ab\\'c' x="\\{ 123 \\}"} world...`
     );
 
-    // t.log(tokens);
+    t.snapshot(tokens, 'dsl.Lexer #1');
+
 
     t.is(lexer, lexer);
 });
 
-test('dsl.Parser parse()', async t => {
+test('dsl.Parser - this.parse()', async t => {
     const lexer = new Lexer();
     const parser = new Parser();
 
@@ -42,35 +43,35 @@ test('dsl.Parser parse()', async t => {
     // t.is(parser, parser);
 });
 
-test('dsl.Parser syntax', async t => {
+test('dsl.Parser - syntax', async t => {
     t.timeout(10000); // Increase timeout for this test
     const lexer = new Lexer();
     const parser = new Parser();
 
     let tokens = lexer.tokenize('.../q={1:5}');
     let ast = parser.parse(tokens);
-    t.is(ast.nodes[1].opcode, 'seq');
-    t.deepEqual(ast.nodes[1].data, [1, 5, 1]);
+    t.is(ast[1].opcode, 'seq');
+    t.deepEqual(ast[1].data, [1, 5, 1]);
 
     tokens = lexer.tokenize('.../q={1:5:-1}');
     ast = parser.parse(tokens);
-    t.is(ast.nodes[1].opcode, 'seq');
-    t.deepEqual(ast.nodes[1].data, [1, 5, -1]);
+    t.is(ast[1].opcode, 'seq');
+    t.deepEqual(ast[1].data, [1, 5, -1]);
 
     tokens = lexer.tokenize('.../q={1:}');
     ast = parser.parse(tokens);
-    t.is(ast.nodes[1].opcode, 'seq');
-    t.deepEqual(ast.nodes[1].data, [1, Number.MAX_SAFE_INTEGER, 1]);
+    t.is(ast[1].opcode, 'seq');
+    t.deepEqual(ast[1].data, [1, Number.MAX_SAFE_INTEGER, 1]);
 
     tokens = lexer.tokenize('.../q={:20}');
     ast = parser.parse(tokens);
-    t.is(ast.nodes[1].opcode, 'seq');
-    t.deepEqual(ast.nodes[1].data, [0, 20, 1]);
+    t.is(ast[1].opcode, 'seq');
+    t.deepEqual(ast[1].data, [0, 20, 1]);
 
 
 });
 
-test('dsl.Parser preprocess', async t => {
+test('dsl.Parser - preprocess', async t => {
     const parser = new Parser();
     let ast = parser.parse('... q={:5 ^1 #apple} w={#doc:7} e={#3} r={20:30 ^doc:9}', 'main');
     // t.log(ast);
@@ -90,6 +91,23 @@ test('dsl.Interpreter', async t => {
 
 
     t.pass();
+})
+
+test('dsl.Interpreter - throw if unhoped loading', async t => {
+    let interpreter = new Interpreter();
+    interpreter.load('... q={:5}');
+    interpreter.ready();
+    t.throws(() => {
+        interpreter.load('... q={:20}', 'doc');
+    }, { message: 'Interpreter is readied. Stop load inputs.' });
+})
+
+test('dsl.Interpreter - throw if circular reference', async t => {
+    let interpreter = new Interpreter();
+    interpreter.load('... q={:5 ^1} w={20: ^2} e={3:9 ^0}');
+    t.throws(() => {
+        interpreter.ready();
+    }, { instanceOf: Interpreter.InterpreterError, message: 'Circular references exist between tags.' });
 })
 
 
