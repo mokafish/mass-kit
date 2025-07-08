84bdce13b479705bd82601940cc9a71cd7a1a716
mokafish <mokafish@users.noreply.github.com>
2025-07-08 13:26:49 +0800

    feat: LinkedList safe remove node

--- test/foo.js
+++ test/foo.js
@@ -1,10 +1,5 @@
 import test from 'ava';
-import * as play from '../play.js'
 
 test('foo', t => {
     t.like({ a: 1, b: 2, c: 1 }, { a: 1, b: 2 });
 });
-
-test('bar', t => {
-    t.true(play)
-})
